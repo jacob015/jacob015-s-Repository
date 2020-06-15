6cfc70c3728b11c861fd11f574c326a0b6f75487
jacob015 <66816737+jacob015@users.noreply.github.com>
2020-06-15 10:21:34 +0900

    Update CardScripts.cs

--- CardScripts.cs
+++ CardScripts.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class CardScripts : MonoBehaviour
 {
-    public static int[] CardCode = new int[100]; //몬스터의 코드
-    public static int[] MonsterHp = new int[100]; //몬스터의 체력 HpSystem
-    public static int[] MonsterPower = new int[100]; //몬스터의 공격력
-    private int MonsterDamageType; //몬스터의 대미지 타입 1:물리2:마법3:고정4:혼합
+    typedef struct CCode
+    {
+    public int CardCode; //몬스터의 코드
+    public int MonsterHp; //몬스터의 체력 HpSystem
+    public int MonsterPower; //몬스터의 공격력
+    public int MonsterDamageType; //몬스터의 대미지 타입 1:물리2:마법3:고정4:혼합
+    }
     
-    //MonsterScripts와 HpSystem과 상호작용 할것
-    void Start()
-    {
-        CardCode[0] = (int)Code.Example;//예시
-        MonsterHp[CardCode[0]] = 100;
-        MonsterPower[CardCode[0]] = 50;
-        
-    }
+    CCode spider;
 
-    void Update()
+    void awake()
     {
-        
-
+    spider.CardCode = 0;
+    spider.MonsterHp = 2;
+    spider.MonstrrPower = 1;
+    spider.MonsterDamageType = 1;
     }
+    
     enum Code
     {
         Example
     };
+    
 }
