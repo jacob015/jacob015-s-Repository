fc7b066875b340bfdc95a2d434b8a82820336eee
jacob015 <66816737+jacob015@users.noreply.github.com>
2020-06-18 17:24:26 +0900

    Update CardScripts.cs

--- CardScripts.cs
+++ CardScripts.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct CardC
+
+[System.Serializable]
+public class CardC
 {
-    public int CardCode;//몬스터의 코드
+    public string CardCode;//몬스터의 코드
     public int MonsterHp;//몬스터의 체력 HpSystem
     public int MonsterPower;//몬스터의 공격력
     public int MonsterCost;
@@ -19,17 +21,23 @@ public class CardScripts : MonoBehaviour
     //MonsterScripts와 HpSystem과 상호작용 할것
     void Awake()
     {
-        CardState[(int)Code.Spider].CardCode = 0;
+        CardState[(int)Code.Spider].CardCode = "0";
         CardState[(int)Code.Spider].MonsterHp = 2;
         CardState[(int)Code.Spider].MonsterPower = 1;
         CardState[(int)Code.Spider].MonsterDamageType = 1;
         CardState[(int)Code.Spider].MonsterCost = 1;
 
-        CardState[(int)Code.StrSpider].CardCode = 1;
+        CardState[(int)Code.StrSpider].CardCode = "1";
         CardState[(int)Code.StrSpider].MonsterHp = 3;
         CardState[(int)Code.StrSpider].MonsterPower = 2;
         CardState[(int)Code.StrSpider].MonsterDamageType = 1;
         CardState[(int)Code.StrSpider].MonsterCost = 2;
+
+        CardState[(int)Code.PowSpider].CardCode = "2";
+        CardState[(int)Code.PowSpider].MonsterHp = 3;
+        CardState[(int)Code.PowSpider].MonsterPower = 2;
+        CardState[(int)Code.PowSpider].MonsterDamageType = 1;
+        CardState[(int)Code.PowSpider].MonsterCost = 2;
 
     }
 
@@ -41,6 +49,6 @@ public class CardScripts : MonoBehaviour
 
     enum Code
     {
-        Spider, StrSpider
+        Spider, StrSpider, PowSpider
     };
 }
